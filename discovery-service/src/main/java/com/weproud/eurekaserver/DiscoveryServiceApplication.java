3960d9455140e2fdaa714b800f1a3020f4cc5b6a
Logan. K <taejung.kim@okiconcession.com>
2017-10-31 11:28:38 +0900

    config-service와 discovery-service연동 완료

--- discovery-service/src/main/java/com/weproud/eurekaserver/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/weproud/eurekaserver/DiscoveryServiceApplication.java
@@ -1,14 +1,26 @@
 package com.weproud.eurekaserver;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@Slf4j
 @EnableEurekaServer
 @SpringBootApplication
-public class DiscoveryServiceApplication {
+public class DiscoveryServiceApplication implements CommandLineRunner {
+
+    @Value("${config-service.test}")
+    private String configServiceMessage;
 
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(final String... args) throws Exception {
+        log.info("config-service is working: {}", this.configServiceMessage);
     }
 }
